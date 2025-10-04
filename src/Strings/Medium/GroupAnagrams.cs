522f52069b2cb4d94038d28efc49f5941e028b04
Christie K <christie.konkol@gmail.com>
2025-10-04 17:32:06 -0600

    Add Group Anagram Comments

--- src/Strings/Medium/GroupAnagrams.cs
+++ src/Strings/Medium/GroupAnagrams.cs
@@ -1,5 +1,17 @@
 ﻿namespace Strings.Medium;
 
+/*
+ *Group Anagrams
+   Write a function that takes in an array of strings and groups anagrams together.
+   Anagrams are strings made up of exactly the same letters, where order doesn't matter.
+   For example, "cinema" and "iceman" are anagrams; similarly, "foo" and "ofo" are anagrams.
+   Your function should return a list of anagram groups in no particular order.
+
+   Sample Input: words = ["yo", "act", "flop", "tac", "foo", "cat", "oy", "olfp"]
+    Sample Output: [["yo", "oy"], ["flop", "olfp"], ["act", "tac", "cat"], ["foo"]]
+
+     O(n * m log(m)) time | O(n * m) space - where n is the number of words and m is the length of the longest word
+ */
 public class GroupAnagrams
 {
     public static List<List<string>> GetGroupAnagrams(List<string> words)
