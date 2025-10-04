0c4311a0f0785658068a6c3b50a3e1cec819185c
Christie K <christie.konkol@gmail.com>
2025-10-04 17:30:33 -0600

    Group Anagrams

---
+++ src/Strings/Medium/GroupAnagrams.cs
@@ -0,0 +1,26 @@
+﻿namespace Strings.Medium;
+
+public class GroupAnagrams
+{
+    public static List<List<string>> GetGroupAnagrams(List<string> words)
+    {
+        var groups = new Dictionary<string, List<string>>();
+
+        foreach (var word in words)
+        {
+            var chars = word.ToCharArray();
+            Array.Sort(chars);
+            var sortedKey = new string(chars);
+
+            if (!groups.TryGetValue(sortedKey, out var group))
+            {
+                group = [];
+                groups[sortedKey] = group;
+            }
+
+            group.Add(word);
+        }
+
+        return groups.Values.ToList();
+    }
+}
---
+++ test/StringsUnitTests/Medium/GroupAnagramsUnitTests.cs
@@ -0,0 +1,62 @@
+﻿using Strings.Medium;
+
+namespace StringsUnitTests.Medium;
+
+public class GroupAnagramsUnitTests
+{
+    [Theory]
+    [MemberData(nameof(GetTestData))]
+    public void TestGetGroupAnagrams(List<string> input, List<List<string>> expectedResult)
+    {
+        var result = GroupAnagrams.GetGroupAnagrams(input);
+        Assert.Equivalent(expectedResult, result);
+    }
+
+    public static TheoryData<List<string>, List<List<string>>> GetTestData
+    {
+        get
+        {
+            var data = new TheoryData<List<string>, List<List<string>>>();
+            data.Add(
+                ["eat", "tea", "tan", "ate", "nat", "bat"],
+                [
+                    new List<string> { "eat", "tea", "ate" },
+                    new List<string> { "tan", "nat" },
+                    new List<string> { "bat" }
+                ]
+            );
+            data.Add(
+                ["a", "b", "a"],
+                [
+                    new List<string> { "a", "a" },
+                    new List<string> { "b" }
+                ]
+            );
+            data.Add(
+                [""],
+                [new List<string> { "" }]
+            );
+            data.Add(
+                ["abc", "def", "ghi"],
+                [
+                    new List<string> { "abc" },
+                    new List<string> { "def" },
+                    new List<string> { "ghi" }
+                ]
+            );
+            data.Add(
+                [],
+                []
+            );
+            data.Add(
+                ["listen", "silent", "hello", "world"],
+                [
+                    new List<string> { "listen", "silent" },
+                    new List<string> { "hello" },
+                    new List<string> { "world" }
+                ]
+            );
+            return data;
+        }
+    }
+}
