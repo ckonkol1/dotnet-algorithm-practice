ed532c87cd8f997256234ef17345cd576c1792fe
Christie K <christie.konkol@gmail.com>
2025-10-05 11:42:51 -0600

    Reverse words in string

---
+++ test/StringsUnitTests/Medium/ReverseWordsInStringUnitTests.cs
@@ -0,0 +1,22 @@
+﻿using Strings.Medium;
+
+namespace StringsUnitTests.Medium;
+
+public class ReverseWordsInStringUnitTests
+{
+    [Theory]
+    [InlineData("hello world", "world hello")]
+    [InlineData("a b c", "c b a")]
+    [InlineData("single", "single")]
+    [InlineData("", "")]
+    [InlineData("  leading spaces", "spaces leading  ")]
+    [InlineData("trailing spaces  ", "  spaces trailing")]
+    [InlineData("  multiple   spaces  between words  ", "  words between  spaces   multiple  ")]
+    [InlineData("punctuation!", "punctuation!")]
+    [InlineData("word1, word2.", "word2. word1,")]
+    public void TestGetReverseWordsInString(string input, string expectedResult)
+    {
+        var result = ReverseWordsInString.GetReverseWordsInString(input);
+        Assert.Equal(expectedResult, result);
+    }
+}
